f7d92624fa9ca564506a01101c3c43c60017606e
KangHyeonSeok <31717201+KangHyeonSeok@users.noreply.github.com>
2022-12-19 06:25:52 +0000

    인자 갯수 체크

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+if(args == null || args.Length < 1) {
+    Console.WriteLine("Useses(Insert line) : TextUtil.ext -insert <filename> <existing statement> <to add statement>");
+    return 1;
+}
+
+return 0;
